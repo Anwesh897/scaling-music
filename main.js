357adcbf196827c0bf9defc82ac50a620e3ad58a
Anwesh <anwesh897@gmail.com>
2019-10-14 07:59:46 +0530

    commit

--- main.js
+++ main.js
@@ -2,42 +2,40 @@ const express = require("express");
 const request = require("request");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
+const path = require("path");
 const dotenv = require("dotenv").config();
-
 const app = express();
+
+const authkey = fs.readFileSync("AuthKey_DX3S9726SR.p8").toString();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Do not go gentle into that good night");
 });
 
 ///Route for the Data which is to be displayed...!
-app.get("/scale", verifyToken, (req, res) => {
+app.post("/scale", verifyToken, (req, res) => {
   jwt.verify(req.token, authkey, (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      request(
-        {
-          //`https://wall.alphacoders.com/api2.0/get.php?auth=${process.env.key}&method`
-          url: `curl -v -H 'Authorization: Bearer ${req.token}' "https://api.music.apple.com/v1/catalog/us/songs/203709340"`
-        },
-        (error, response, body) => {
-          if (error || response.statusCode !== 200) {
-            res.status(500).json({ type: "error", msg: err.message });
-          }
-          const data = JSON.parse(body);
-          res.json({
-            data: data,
-            auth: authData
-          });
+    request(
+      {
+        url:
+          //" https://wall.alphacoders.com/api2.0/get.php?auth=02600714b6a89846733ece084dbc7e23&method"
+          "https://api.music.apple.com/v1/catalog/us/songs/203709340"
+        //"https://api.music.apple.com/v1/test"
+      },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: "error", message: err.message });
         }
-      );
-    }
+        const data = JSON.parse(body);
+        res.json(data);
+      }
+    );
   });
 });
 
 ///Protecting the above Route
-const authkey = fs.readFileSync("AuthKey_DX3S9726SR.p8").toString();
 app.post("/scale/token", (req, res) => {
   jwt.sign(
     {},
@@ -48,10 +46,10 @@ app.post("/scale/token", (req, res) => {
       issuer: process.env.issuer,
       header: {
         alg: "ES256",
+        typ: "JWT",
         kid: process.env.kid
       }
     },
-    // { kid: process.env.kid },
     (err, token) => {
       if (err) {
         res.json({ err: err.message });
---
+++ public/index.js
@@ -0,0 +1,11 @@
+const url = "http://localhost:4321/scale";
+
+getData();
+async function getData() {
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "cors"
+  });
+  const data = await response.json();
+  console.log(data);
+}
